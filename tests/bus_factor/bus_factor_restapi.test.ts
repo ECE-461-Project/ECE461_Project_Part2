7c21c9e3d8ffb7e7e5dc97feeb4d6484df08262b
brewe116 <89466466+brewe116@users.noreply.github.com>
2023-02-10 23:37:30 -0500

    Fixed the value for testing

--- tests/bus_factor/bus_factor_restapi.test.ts
+++ tests/bus_factor/bus_factor_restapi.test.ts
@@ -9,7 +9,7 @@ describe('testing get_percent_owner', () => {
   test('should return the percent of commits contributed by the owner', async () => {
     expect(
       await get_percent_owner('https://github.com/torvalds/linux')
-    ).toBeGreaterThan(10000000000000000);
+    ).toBeGreaterThan(0);
   });
   test('should return undefined since not github', async () => {
     expect(await get_percent_owner('https://github./torvalds/linux')).toBe(
