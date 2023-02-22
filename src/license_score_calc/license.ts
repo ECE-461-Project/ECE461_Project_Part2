a6226204b68b754a6f68bf415e71bda46e6ef1b9
Justin Lee <just007in2000@gmail.com>
2023-02-22 18:17:51 -0500

    updated license checker to check package.json only

--- src/license_score_calc/license.ts
+++ src/license_score_calc/license.ts
@@ -1,17 +1,37 @@
-import {check_licenses_result} from './license_util';
+import {readFile} from 'fs/promises';
+import {join} from 'path';
 
+// Example of using promise using async
+// https://janelia-flyem.github.io/licenses.html
+// https://en.wikipedia.org/wiki/ISC_license
+// https://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License#Differences_from_the_GPL
+// Only allow the following licenses in module and dependencies:
+//  MIT, Apache, ISC, WTFPL, BSD, BSD-Source-Code, CC0-1.0, Public Domain, LGPL-2.1-only, CC-BY-*
+//  This uses SPDX Identifiers
+// Does NOT handle custom or unlicenses modules. Defaults them to invalid for now.
 export async function get_license_score(
   repo_url: string,
   local_repo_path: string
 ): Promise<number> {
-  const is_valid = await check_licenses_result(local_repo_path);
-  globalThis.logger.info(`license status for ${repo_url}: ${is_valid}`);
+  try {
+    const package_json = JSON.parse(
+      (await readFile(join(local_repo_path, 'package.json'))).toString()
+    );
+    const license: string | undefined = package_json.license;
+    globalThis.logger.info(`${repo_url} has license: ${license}`);
 
-  const score = is_valid ? 1 : 0;
-
-  return score;
+    const license_regex = new RegExp(
+      'MIT|Apache|ISC|WTFPL|BSD|BSD-Source-Code|CC0-1.0|Public Domain|LGPL-2.1-only|CC-BY-*'
+    );
+    if (license) {
+      return license_regex.exec(license) ? 1 : 0;
+    } else {
+      return 0;
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      globalThis.logger.error(`License Score calc got error: ${err.message}`);
+    }
+  }
+  return 0;
 }
-
-//get_license_score().then((data: number) => {
-//  console.log(data);
-//});
