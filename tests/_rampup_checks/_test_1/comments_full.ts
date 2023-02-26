10715a26a4178bf5704cc78b4dfb8659b0b79e68
Priyanka Pereira <papereir@purdue.edu>
2023-02-26 17:44:20 -0500

    fixed tests to be on simpler files, recurse still

---
+++ tests/_rampup_checks/_test_1/comments_full.ts
@@ -0,0 +1,14 @@
+/*
+ *
+ *
+ * Random empty .ts file to check
+ *
+ *
+ */
+
+let sum = 0;
+const a = 3;
+const b = 38;
+sum = a + b;
+// print the sum
+console.log(sum);
---
+++ tests/_rampup_checks/_test_1/recurse_check/non_comments.js
@@ -0,0 +1,7 @@
+// Random empty .js file to check
+
+let wow = 0;
+const c = 3;
+const d = 38;
+wow = c + d;
+console.log(wow);
