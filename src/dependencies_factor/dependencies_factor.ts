ad93dca1d335a2f5e956ce1bc1f8088b777a28b1
Priyanka Pereira <papereir@purdue.edu>
2023-03-02 17:25:42 -0500

    New regex, altered for async fs calls:

--- src/dependencies_factor/dependencies_factor.ts
+++ src/dependencies_factor/dependencies_factor.ts
@@ -1,9 +1,11 @@
-import {readFileSync} from 'fs';
+import {readFile} from 'fs/promises';
 import {join} from 'path';
 
 export function check_if_pinned(dependency_version: string): boolean {
   console.log(dependency_version);
-  const pinned_regex = /^=*\d+\.\d+/gm;
+  //const pinned_regex = /^=*\d+\.\d+/gm;
+  const pinned_regex =
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
   const match = dependency_version.match(pinned_regex);
   console.log(match);
   if (match === null) {
@@ -19,19 +21,18 @@ export async function get_dependencies_score(
 ): Promise<number> {
   try {
     const package_json = JSON.parse(
-      readFileSync(join(local_repo_path, 'package.json')).toString()
+      (await readFile(join(local_repo_path, 'package.json'))).toString()
     );
     const check_exists: string | undefined = package_json.dependencies;
     if (check_exists !== null && check_exists !== undefined) {
       const dependencies = JSON.parse(
         JSON.stringify(package_json.dependencies)
       );
-      globalThis.logger?.info(`${repo_url} has dependencies!`);
+
       let num_dependencies = 0;
       let num_pinned_dependencies = 0;
       for (const dependency in dependencies) {
-        // eslint-disable-next-line no-prototype-builtins
-        if (dependencies.hasOwnProperty(dependency)) {
+        if (Object.prototype.hasOwnProperty.call(dependencies, dependency)) {
           num_dependencies++;
           if (check_if_pinned(dependencies[dependency])) {
             num_pinned_dependencies++;
@@ -40,6 +41,9 @@ export async function get_dependencies_score(
       }
       console.log(num_dependencies);
       console.log(num_pinned_dependencies);
+      globalThis.logger?.info(
+        `${repo_url} has ${num_dependencies} dependencies and ${num_pinned_dependencies} pinned!`
+      );
       if (num_pinned_dependencies > 0) {
         return 1 / num_pinned_dependencies;
       } else {
@@ -51,8 +55,10 @@ export async function get_dependencies_score(
   } catch (err) {
     if (err instanceof Error) {
       console.log(err.message);
-      globalThis.logger?.error(`License Score calc got error: ${err.message}`);
+      globalThis.logger?.error(
+        `Dependencies Score calc got error, returning 1: ${err.message}`
+      );
     }
-    return 0;
+    return 1;
   }
 }
