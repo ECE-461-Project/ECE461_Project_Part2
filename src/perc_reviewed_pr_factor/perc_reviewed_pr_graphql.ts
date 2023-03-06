597345a545da978d42874d0bdbcb4f7401d6c4a1
Priyanka Pereira <papereir@purdue.edu>
2023-03-06 16:51:38 -0500

    first query for commit count and pagination of PRs works

--- src/perc_reviewed_pr_factor/perc_reviewed_pr_graphql.ts
+++ src/perc_reviewed_pr_factor/perc_reviewed_pr_graphql.ts
@@ -51,28 +51,99 @@ export async function fetch_graphql_data(
   if (owner_repo === undefined) {
     return undefined;
   }
-  let result: GraphQlQueryResponseData | undefined = undefined;
-  result = undefined;
+
   try {
-    const allContributions = await graphql({
-      query: `query NewQuery($repo: String!, $owner: String!) {
-				  repository(owner: $owner, name: $repo) {
-				    object(expression: "master") {
-				      ... on Commit {
-				        history {
-				          totalCount
-				        }
-				      }
-				    }
-				  }
-				}`,
+    let query_result: GraphQlQueryResponseData | undefined = undefined;
+    let tot_commit_count = 0;
+    query_result = await graphql({
+      query: `query PR_TOTCOMMIT_Query($owner: String!, $repo: String!) {
+  repository(owner: $owner, name: $repo) {
+    object(expression: "master") {
+      ... on Commit {
+        history {
+          totalCount
+        }
+      }
+    }
+  }
+}`,
       owner: owner_repo.owner,
       repo: owner_repo.repo,
       headers: {
         authorization: 'bearer ' + process.env.GITHUB_TOKEN,
       },
     });
-    console.log(allContributions);
+    if (query_result !== undefined) {
+      const jdata = JSON.parse(JSON.stringify(query_result));
+      tot_commit_count = jdata.repository.object.history.totalCount;
+      console.log(tot_commit_count);
+    } else {
+      globalThis.logger?.info(
+        '%PR Factor: could not get total commit count of repo - returning 0.'
+      );
+      return 0;
+    }
+    if (tot_commit_count === 0) {
+      globalThis.logger?.info(
+        '%PR Factor: Total commit count of repo is 0 - returning 0.'
+      );
+      return 0;
+    }
+
+    // find commit count of PR/Reviewed commits (first pagination without loop, first 100)
+    let query_result_pre_loop: GraphQlQueryResponseData | undefined = undefined;
+    let tot_commit_with_reviewed_pr = 0;
+    let tot_pr_count = 0;
+    let has_next_page = false;
+    let end_cursor = "";
+    query_result_pre_loop = await graphql({
+      query: `query PR_REAL_FIRST_Query($owner: String!, $repo: String!) {
+  repository(owner: $owner, name: $repo) {
+    pullRequests(states: MERGED, first: 100) {
+      totalCount
+      nodes {
+        reviews(states: APPROVED) {
+          totalCount
+        }
+        commits {
+          totalCount
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}`,
+      owner: owner_repo.owner,
+      repo: owner_repo.repo,
+      headers: {
+        authorization: 'bearer ' + process.env.GITHUB_TOKEN,
+      },
+    });
+    if (query_result_pre_loop !== undefined) {
+      const jdata = JSON.parse(JSON.stringify(query_result_pre_loop));
+      tot_pr_count = jdata.repository.pullRequests.totalCount;
+      has_next_page = jdata.repository.pullRequests.pageInfo.hasNextPage;
+      end_cursor = jdata.repository.pullRequests.pageInfo.endCursor;
+      console.log(tot_pr_count);
+      console.log(typeof has_next_page);
+      console.log(has_next_page);
+      console.log(typeof end_cursor);
+      console.log(end_cursor);
+    } else {
+      globalThis.logger?.info(
+        '%PR Factor: could not get total PR count of repo - returning 0.'
+      );
+      return 0;
+    }
+    if (tot_pr_count === 0) {
+      globalThis.logger?.info(
+        '%PR Factor: Total PR count of repo is 0 - returning 0.'
+      );
+      return 0;
+    }
   } catch (error) {
     if (error instanceof GraphqlResponseError) {
       globalThis.logger?.error(
@@ -83,6 +154,7 @@ export async function fetch_graphql_data(
         '%PR Factor: GraphQL call failed for reason unknown!'
       );
     }
+    return 0;
   }
   console.log(owner_repo);
   return 1;
