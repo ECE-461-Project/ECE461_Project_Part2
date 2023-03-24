d039c3ddd55d0e137d7402c752146158010963e2
Justin Lee <just007in2000@gmail.com>
2023-03-24 17:21:31 -0400

    updated to ignore .git directory in zipping

--- src/api_server/zip_files.ts
+++ src/api_server/zip_files.ts
@@ -1,6 +1,7 @@
 import {getAllFiles} from './get_files';
 import JSZip = require('jszip');
 import {readFile} from 'fs/promises';
+import {createWriteStream} from 'fs';
 // ***************************
 // This ensures jszip compiles
 // https://stackoverflow.com/questions/66275648/aws-javascript-sdk-v3-typescript-doesnt-compile-due-to-error-ts2304-cannot-f
@@ -13,28 +14,30 @@ declare global {
 }
 // ****************************
 
+// Ignores .git folder
 export async function generate_base64_zip_of_dir(
   directory: string
 ): Promise<string> {
+  const ignore_git = RegExp('^\\.git/');
   const zip = new JSZip();
   const array_of_files = await getAllFiles(directory);
-
   // If we want to do reading in parallel in the future:
   //  https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
   for (const file of array_of_files) {
-    const fileContent = await readFile(file, 'utf-8');
-    zip.file(file, fileContent, {createFolders: true});
+    if (!ignore_git.exec(file)) {
+      const fileContent = await readFile(file, 'utf-8');
+      zip.file(file, fileContent, {createFolders: true});
+    }
   }
   // Should we do compression:
   //  https://stuk.github.io/jszip/documentation/api_jszip/generate_async.html#compression-and-compressionoptions-options
   //  NOTE: Default is already compressed!
   return zip.generateAsync({type: 'base64'});
-
+  /*
   // If you want to stream zip to filesystem
   // https://stuk.github.io/jszip/documentation/howto/write_zip.html
   // From jszip doc
   // For manually validation
-  /*
   zip
     .generateNodeStream({type: 'nodebuffer', streamFiles: true})
     .pipe(createWriteStream('out.zip'))
@@ -43,5 +46,12 @@ export async function generate_base64_zip_of_dir(
       // but is piped here in a writable stream which emits a "finish" event.
       console.log('out.zip written.');
     });
-  */
+    return '';
+    */
 }
+/*
+async function main() {
+  console.log(await generate_base64_zip_of_dir('./'));
+}
+main();
+*/
