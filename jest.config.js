567e6b312f8cc7daeb3f1f557f90f75e1f404b77
Justin Lee <just007in2000@gmail.com>
2023-03-28 20:35:54 -0400

    fixed linter errors

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules']
+  testPathIgnorePatterns: ['/node_modules'],
 };
