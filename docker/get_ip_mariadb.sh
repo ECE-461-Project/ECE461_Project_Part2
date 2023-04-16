bafe7f8315eb0cf31e5b0d4dcb2b59d957eb1540
Justin Lee <just007in2000@gmail.com>
2023-04-16 15:33:09 -0400

    removed extra files, downgraded postgres version to 14 from 15 to match gcp

--- docker/get_ip_mariadb.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# https://mariadb.com/kb/en/installing-and-using-mariadb-via-docker/
-# Hopefully this is unnecessary and can just use localhost
-sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb_container
