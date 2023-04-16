e7894aa451a7409f3c29c22a0f1619b40d256acf
Justin Lee <just007in2000@gmail.com>
2023-04-16 15:27:44 -0400

    removed mariadb, using postgres using pg npm package, changed .env.mariadb to .env.database

--- docker/remove_volumes.sh
+++ docker/remove_volumes.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker volume ls | grep "mariadb_volume1" | awk '{print $2}' | xargs sudo docker volume rm
+sudo docker volume ls | grep "postgres_volume1" | awk '{print $2}' | xargs sudo docker volume rm
