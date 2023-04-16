d81ff720370730ecae565edf8165612c8bb8c78e
vcxrant <vickrant.code@gmail.com>
2023-04-16 13:22:15 -0400

    update interface to match spec

--- src/api_server/models/models.ts
+++ src/api_server/models/models.ts
@@ -279,4 +279,7 @@ export type EnumerateOffset = string;
  * A regular expression over package names and READMEs that is used for searching for a package.
  * @export
  */
-export type PackageRegEx = string;
+export interface PackageRegEx {
+  Version: string;
+  Name: string;
+}
